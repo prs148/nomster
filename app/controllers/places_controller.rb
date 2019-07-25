1131f91e5c215f4ae395a263bbabff38aa04ac72
Pete <prs148@hotmail.com>
2019-07-25 01:36:33 +0000

    form outline

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -2,8 +2,12 @@ class PlacesController < ApplicationController
 
   def index 
     @places = Place.order("name").page(params[:page]).per_page(10)
-
-     
   end
+
+
+  def new 
+    @place = Place.new 
+  end 
+
 
 end
