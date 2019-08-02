b6e419bb0151482f56b407f1b953ca9c84462f2b
Pete <prs148@hotmail.com>
2019-08-02 18:47:14 +0000

    deleted extra migration files

--- db/migrate/20190728212950_add_user_and_index_to_photo.rb
+++
@@ -1,7 +0,0 @@
-class AddUserAndIndexToPhoto < ActiveRecord::Migration[5.2]
-  def change
-    add_column :photos, :user_id, :integer
-   
-  end
-
-end
--- db/migrate/20190728213711_add_index_to_photo.rb
+++
@@ -1,6 +0,0 @@
-class AddIndexToPhoto < ActiveRecord::Migration[5.2]
-  def change
-    add_index :photos, [:user_id, :place_id]
-    add_index :photos, :place_id 
-  end
-end
