c0bbdd9c7e8b62dfa8ed4213a6ba4de265b1d5dc
Pete <prs148@hotmail.com>
2019-08-20 03:37:58 +0000

    complete for review

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -11,15 +11,7 @@ Rails.application.configure do
  #    password: ENV['GMAIL_PASSWORD']
 #}
 
- ActionMailer::Base.smtp_settings = {
-    address: 'smtp.sendgrid.net',
-    port: '587',
-    authentication: :plain,
-    user_name: ENV['SENDGRID_USERNAME'],
-    password: ENV['SENDGRID_PASSWORD'],
-    domain: 'heroku.com',
-    enable_starttls_auto: true
-  }
+ 
    config.action_mailer.default_url_options = { host: 'localhost:3030' }
    config.action_mailer.delivery_method = :test 
 
